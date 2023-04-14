7d73eddffea4442c778d2a08b201cc3b7d1552a2
Soumil Singh <soumil.sps@gmail.com>
2023-04-15 01:25:09 +0530

    added topic backend

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {createnewTopic,getUserFollowedTopicsTest} = require('../database/topic.js');
 const {createnewPost, getUserPostTest} = require('../database/post.js');
+const mongoose = require("../config/db");
+const Topic = require('../models/topic.js');
+const Post = require('../models/post.js');
+
+router.get("/", (req, res) => {
+    res.redirect('/home');
+});
 
 router.get('/home', (req, res) => {
-    console.log(req.isAuthenticated());
     if(req.isAuthenticated()) {
         const topics = getUserFollowedTopicsTest('123');
         const posts = getUserPostTest("req.user.id");
@@ -12,6 +18,24 @@ router.get('/home', (req, res) => {
     }
     else {
         res.redirect('/auth/login');
+    }
+});
+
+router.get("/topic/:topicName", (req, res) => {
+    const topicName = req.params.topicName;
+
+    if (req.isAuthenticated()) {
+        Topic.find({topicName: topicName}, (err, topic) => {
+            if(err) console.log(err);
+            
+            else {
+                let posts = [];
+                topic.topicPosts.forEach(post => {
+                    posts.push(Post.findOne({_id: post}));
+                })
+                res.render('topic.ejs', {topic: topic, posts: posts});
+            }
+        });
     }
 });
 
