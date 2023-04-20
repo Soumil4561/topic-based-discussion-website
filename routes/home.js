8784698649f0d5e70289df42f7a2b427c6fde89b
Manish Gudewar <202152322@iiitvadodara.ac.in>
2023-04-20 16:24:41 +0530

    profile photu

--- routes/home.js
+++ routes/home.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const mongoose = require("../config/db");
-const Topic = require('../models/topic.js');
-const Post = require('../models/post.js');
-const User = require('../models/user.js');
-const {getUserFollowedTopics, getPosts} = require('../controllers/home.js');
+const Topic = require("../models/topic.js");
+const Post = require("../models/post.js");
+const User = require("../models/user.js");
+const { getUserFollowedTopics, getPosts } = require("../controllers/home.js");
 
 router.get("/", (req, res) => {
-    res.redirect('/home');
+  res.redirect("/home");
 });
 
-router.get('/home', async (req, res) => {
-    const user = User.findOne({_id: req.user.id});
-    if(req.isAuthenticated()) {
-        const topics = await getUserFollowedTopics(req.user.id);
-        const posts = await getPosts(topics);
-        res.render('home', {topics: topics, posts: posts, user: user});
-    }
-    else {
-        res.redirect('/auth/login');
-    }
+router.get("/home", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const user = await User.findOne({ _id: req.user.id });
+    const topics = await getUserFollowedTopics(req.user.id);
+    const posts = await getPosts(topics);
+    res.render("home", { topics: topics, posts: posts, user: user });
+  } else {
+    res.redirect("/auth/login");
+  }
 });
 
 module.exports = router;
