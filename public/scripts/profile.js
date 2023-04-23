9402fc7b5046b2e73c0475459627188630f62022
Shubham Gujarathi <202151153@iiitvadodara.ac.in>
2023-04-23 14:28:55 +0530

    editPost initial

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -16,4 +16,24 @@ $(".delete").on("click", function(event) {
         });
     } 
 });
+
+$(".edit").on("click", function(event) {
+    const classList = event.target.classList;
+    if(classList.contains("edit")) {
+        const postID = $(event.target).attr('id');
+        var options = {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({postID: postID})
+        };
+        fetch("/post/"+postID, options).then(res => {
+            if(res.status == 200){
+                res.json().then(data => {
+                    $("."+postID).remove();
+                })
+            }
+        });
+    } 
+});
+
     //appen as url to post image });
